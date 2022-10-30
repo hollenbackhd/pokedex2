1b9a2dfc7e6efa12475f2fc915bd24d0d5355e55
Hannah Hollenback <hollenbackhd@gmail.com>
2022-10-29 20:36:57 -0600

    Finished the end game pieces. Next will work on game mechanics

--- app/src/main/java/com/example/pokedex2/EndFragment.kt
+++ app/src/main/java/com/example/pokedex2/EndFragment.kt
@@ -1,4 +1,48 @@
 package com.example.pokedex2
 
-class EndFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.pokedex2.databinding.FragmentEndBinding
+
+class EndFragment : Fragment() {
+    private var binding: FragmentEndBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentBinding = FragmentEndBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+            restart.setOnClickListener { goToStart() }
+        }
+
+
+    }
+    fun goToStart(){
+        findNavController().navigate(R.id.action_endFragment_to_startFragment)
+    }
+
+    /**
+     * Start an order with the desired quantity of cupcakes and navigate to the next screen.
+     */
+
+
+    /**
+     * This fragment lifecycle method is called when the view hierarchy associated with the fragment
+     * is being removed. As a result, clear out the binding object.
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
 }
