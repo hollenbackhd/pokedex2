42656405644255aaa3075723c5e3730c01344b44
Hannah Hollenback <hollenbackhd@gmail.com>
2022-12-04 17:36:25 -0700

    Added a new add_score fragment, but I'm not sure if I really want to add the scores in this way

--- app/src/main/java/com/example/pokedex2/AddScoreFragment.kt
+++ app/src/main/java/com/example/pokedex2/AddScoreFragment.kt
@@ -4,23 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.pokedex2.databinding.FragmentStartBinding
 import androidx.navigation.fragment.findNavController
+import com.example.pokedex2.databinding.FragmentAddScoreBinding
+import com.example.pokedex2.databinding.FragmentStartBinding
 
-class StartFragment : Fragment() {
-
+class AddScoreFragment : Fragment() {
     // Binding object instance corresponding to the fragment_start.xml layout
     // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
     // when the view hierarchy is attached to the fragment.
-    private var binding: FragmentStartBinding? = null
+    private var binding: FragmentAddScoreBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val fragmentBinding = FragmentStartBinding.inflate(inflater, container, false)
+        val fragmentBinding = FragmentAddScoreBinding.inflate(inflater, container, false)
         binding = fragmentBinding
         return fragmentBinding.root
     }
@@ -28,26 +27,22 @@ class StartFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding?.apply {
-            startGame.setOnClickListener { startGame() }
+            skip.setOnClickListener { startGame() }
         }
         binding?.apply{
-            homePage.setOnClickListener { goHome() }
+            submit.setOnClickListener{ submitScore() }
         }
-        binding?.apply{
-            viewScores.setOnClickListener { viewScores()}
-        }
+
 
 
     }
     fun startGame() {
-        findNavController().navigate(R.id.action_startFragment_to_gameFragment2)
+        findNavController().navigate(R.id.action_addScoreFragment_to_gameFragment)
     }
-    fun goHome(){
-        findNavController().navigate(R.id.action_startFragment_to_endFragment)
+    fun submitScore(){
+        findNavController().navigate(R.id.action_addScoreFragment_to_scoreboardFragment)
     }
-    fun viewScores(){
-        findNavController().navigate(R.id.action_startFragment_to_scoreboardFragment)
-    }
+
 
     /**
      * Start an order with the desired quantity of cupcakes and navigate to the next screen.
