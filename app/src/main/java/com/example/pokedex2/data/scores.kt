c095eed6db85c49703eeb6f446a173d90ea4383a
Hannah Hollenback <hollenbackhd@gmail.com>
2022-12-12 18:36:25 -0700

    Updated scores so that id would autogenerate

--- app/src/main/java/com/example/pokedex2/data/scores.kt
+++ app/src/main/java/com/example/pokedex2/data/scores.kt
@@ -6,7 +6,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 @Entity
 data class scores(
-    @PrimaryKey val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @NonNull @ColumnInfo(name = "user_name") val userName: String,
     @NonNull @ColumnInfo(name = "user_score") val userScore: Int
 )
