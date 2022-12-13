2bc5057a848d60c83d5df89019942869ba2bc4cc
Hannah Hollenback <hollenbackhd@gmail.com>
2022-12-12 19:10:09 -0700

    Updated the scoreboardviewmodel to include more functions

--- app/src/main/java/com/example/pokedex2/ScoreboardViewModel.kt
+++ app/src/main/java/com/example/pokedex2/ScoreboardViewModel.kt
@@ -2,12 +2,30 @@ package com.example.pokedex2
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.example.pokedex2.data.ScoresDao
 import com.example.pokedex2.data.scores
+import kotlinx.coroutines.launch
 
 class ScoreboardViewModel(private val scoresDao: ScoresDao): ViewModel() {
 
     fun fullScores(): List<scores> = scoresDao.getAll()
+
+    fun addNewScore(userName: String, userScore: Int) {
+        val newScore = getNewScoreEntry(userName, userScore)
+        insertScore(newScore)
+    }
+    private fun insertScore(scores: scores) {
+        viewModelScope.launch {
+            scoresDao.insert(scores)
+        }
+    }
+    private fun getNewScoreEntry(userName: String, userScore: Int): scores {
+        return scores(
+            userName = userName,
+            userScore = userScore
+        )
+    }
 
 }
 
